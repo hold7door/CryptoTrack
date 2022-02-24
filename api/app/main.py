82eca650d02801c00df19603da53ab912fb54d33
arpit <arpitpathak97@gmail.com>
2022-02-24 11:58:04 +0530

    update api

--- api/app/main.py
+++ api/app/main.py
@@ -3,11 +3,16 @@ import aioredis
 
 from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseSettings
+from pydantic import BaseSettings, BaseModel
+
+from datetime import datetime, timedelta
 
 class Config(BaseSettings):
     # The default URL expects the app to run in a docker container
     redis_url: str = 'redis://localhost:6379'
+
+class Query(BaseModel):
+    time_range: str
 
 
 log = logging.getLogger(__name__)
@@ -20,10 +25,39 @@ app = FastAPI(title='CryptoTrack API server')
    
     log.info('Connected to redis')
  """
-@app.get('/daily')
-async def get_daily():
-    result = await redis.zrangebyscore('crypto_daily_BTC', '-inf', 'inf')
-    print(result)
+@app.post('/daily')
+async def get_daily(query: Query):
+    print(query)
+    time_range = query.time_range
+    
+    timestamps = []
+
+    if time_range == '7d':
+        now = datetime.now()
+        delta_day = timedelta(1)
+        delta_sec = timedelta(0, 1)
+        for _ in range(7):
+            current_timestamp = datetime.timestamp(now)
+            prev_date = now - delta_day
+            prev_timestamp = datetime.timestamp(prev_date)
+            timestamps.append((prev_timestamp, current_timestamp))
+            now = prev_date - delta_sec
+
+    print(timestamps)
+
+    result = {
+        'BTC': [],
+        'ETH': []
+    }
+
+    for crypto in ['BTC', 'ETH']:
+        for mn, mx in timestamps:
+            values = await redis.zrangebyscore(f'crypto_daily_{crypto}', mn, mx)
+            sm = 0
+            for value in values:
+                sm += int(value.split('-')[0])
+            result[crypto].append(sm)
+
     return {
         'result': result
     }
--- scraping/scraping/spiders/crypto_scraper.py
+++ scraping/scraping/spiders/crypto_scraper.py
@@ -27,17 +27,17 @@ class CryptoScraper(Spider):
             headline = d.xpath(".//a[contains(@class, 'headline')]/text()").extract()
             content = d.xpath(".//span[contains(@class, 'content-text')]/text()").extract()
             publish_date = d.xpath(".//div[contains(@class, 'ac-publishing-date')]/span/text()").extract()
-
+            
             if len(publish_date) > 0:
                 publish_date = publish_date[0]
                 publish_date_obj = dateTimeFromString(publish_date)
-
                 if publish_date_obj and getHoursDiff(publish_date_obj, current_datetime) < 1:
                     headline = headline[0] if len(headline) > 0 else ''
                     content = content[0] if len(content) > 0 else ''
                     headline_keywords = [w.lower() for w in  headline.split()]
                     content_keywords = [w.lower() for w in content.split()]
                     all_keywords = set(headline_keywords + content_keywords)
+                    print (all_keywords)
                     for crypto_name in CRYPTOS.keys():
                         for variants in CRYPTOS[crypto_name]:
                             if variants.lower() in all_keywords:
